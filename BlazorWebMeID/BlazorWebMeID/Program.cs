683b04cda218c753f0b6f17424b30924274859cb
damienbod <damien_bod@hotmail.com>
2024-02-03 14:38:17 +0100

    Working example using Auth0

---
+++ BlazorWebMeID/BlazorWebMeID/Program.cs
@@ -0,0 +1,169 @@
+using BlazorWebMeID.Client;
+using BlazorWebMeID.Client.Pages;
+using BlazorWebMeID.Client.Services;
+using BlazorWebMeID.Components;
+using BlazorWebMeID.Identity;
+using BlazorWebMeID.Services;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+builder.Services.AddCascadingAuthenticationState();
+builder.Services.AddScoped<AuthenticationStateProvider, PersistingRevalidatingAuthenticationStateProvider>();
+
+// Add authentication services
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+})
+.AddCookie(options =>
+{
+    options.Cookie.Name = "__Host-BlazorServer";
+    options.Cookie.SameSite = SameSiteMode.Lax;
+})
+.AddOpenIdConnect(OpenIdConnectDefaults.AuthenticationScheme, options =>
+{
+    options.Authority = $"https://{builder.Configuration["Auth0:Domain"]}";
+    options.ClientId = builder.Configuration["Auth0:ClientId"];
+    options.ClientSecret = builder.Configuration["Auth0:ClientSecret"];
+    options.ResponseType = OpenIdConnectResponseType.Code;
+    options.Scope.Clear();
+    options.Scope.Add("openid");
+    options.Scope.Add("profile");
+    options.Scope.Add("email");
+    options.Scope.Add("auth0-user-api-one");
+    options.CallbackPath = new PathString("/signin-oidc");
+    options.ClaimsIssuer = "Auth0";
+    options.SaveTokens = true;
+    options.UsePkce = true;
+    options.GetClaimsFromUserInfoEndpoint = true;
+    options.TokenValidationParameters.NameClaimType = "name";
+
+    options.Events = new OpenIdConnectEvents
+    {
+        // handle the logout redirection 
+        OnRedirectToIdentityProviderForSignOut = (context) =>
+        {
+            var logoutUri = $"https://{builder.Configuration["Auth0:Domain"]}/v2/logout?client_id={builder.Configuration["Auth0:ClientId"]}";
+
+            var postLogoutUri = context.Properties.RedirectUri;
+            if (!string.IsNullOrEmpty(postLogoutUri))
+            {
+                if (postLogoutUri.StartsWith("/"))
+                {
+                    // transform to absolute
+                    var request = context.Request;
+                    postLogoutUri = request.Scheme + "://" + request.Host + request.PathBase + postLogoutUri;
+                }
+                logoutUri += $"&returnTo={Uri.EscapeDataString(postLogoutUri)}";
+            }
+
+            context.Response.Redirect(logoutUri);
+            context.HandleResponse();
+
+            return Task.CompletedTask;
+        },
+        OnRedirectToIdentityProvider = context =>
+        {
+            // The context's ProtocolMessage can be used to pass along additional query parameters
+            // to Auth0's /authorize endpoint.
+            // 
+            // Set the audience query parameter to the API identifier to ensure the returned Access Tokens can be used
+            // to call protected endpoints on the corresponding API.
+            context.ProtocolMessage.SetParameter("audience", "https://auth0-api1");
+
+            return Task.FromResult(0);
+        }
+    };
+});
+
+
+builder.Services.AddSingleton<IAuthorizationMiddlewareResultHandler, BlazorAuthorizationMiddlewareResultHandler>();
+builder.Services.AddScoped<HostingEnvironmentService>();
+builder.Services.AddSingleton<BaseUrlProvider>();
+builder.Services.AddHttpContextAccessor();
+
+builder.Services
+    .AddTransient<CookieHandler>()
+    .AddScoped(sp => sp
+        .GetRequiredService<IHttpClientFactory>()
+        .CreateClient("API"))
+    .AddHttpClient("API", (provider, client) =>
+    {
+        // Get base address
+        var uri = provider.GetRequiredService<BaseUrlProvider>().BaseUrl;
+        client.BaseAddress = new Uri(uri);
+    }).AddHttpMessageHandler<CookieHandler>();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapGet("/Account/Login", async (HttpContext httpContext, string returnUrl = "/") =>
+{
+    await httpContext.ChallengeAsync(OpenIdConnectDefaults.AuthenticationScheme, 
+        new AuthenticationProperties
+        {
+            RedirectUri = !string.IsNullOrEmpty(returnUrl) ? returnUrl : "/"
+        });
+});
+
+app.MapGet("/Account/Logout", async (HttpContext httpContext, string returnUrl = "/") =>
+{
+    var authenticationProperties = new AuthenticationProperties
+    {
+        RedirectUri = "/"
+    };
+    if (httpContext.Request.Cookies.Count > 0)
+    {
+        var siteCookies = httpContext.Request.Cookies.Where(c => c.Key.Contains(".AspNetCore.") || c.Key.Contains("Microsoft.Authentication"));
+        foreach (var cookie in siteCookies)
+        {
+            httpContext.Response.Cookies.Delete(cookie.Key);
+        }
+    }
+
+    await httpContext.SignOutAsync(OpenIdConnectDefaults.AuthenticationScheme, authenticationProperties);
+    await httpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+})
+    .RequireAuthorization();
+
+app.MapGet("/Hello", (HttpContext httpContext) => Results.Ok("Hi!"))
+   .RequireAuthorization();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(Counter).Assembly);
+
+app.Run();
---
+++ BlazorWebMeID/BlazorWebMeID/Services/BaseUrlProvider.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorWebMeID.Services;
+
+public class BaseUrlProvider
+{
+    public string BaseUrl { get; private set; }
+
+    public void SetBaseUrl(string baseUrl)
+    {
+        BaseUrl = baseUrl;
+    }
+}
